360e0adaba85a314ec8cd65a41061623155f0b64
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-06-16 09:11:30 +0300

    fixed stuffy stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { PetsController } from './pets/pets.controller';
-import { PetsService } from './pets/pets.service';
 import { PetsModule } from './pets/pets.module';
-import { ItemsController } from './items/items.controller';
 import { ItemsModule } from './items/items.module';
-import { ItemsService } from './items/items.service';
-import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [UsersModule, PetsModule, ItemsModule],
-  controllers: [AppController, UsersController, PetsController, ItemsController],
-  providers: [AppService, PetsService, ItemsService, UsersService],
+  imports: [TypeOrmModule.forRoot(), UsersModule, PetsModule, ItemsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-@Module({
-  imports: [TypeOrmModule.forRoot()],
-})
-
-export class AppModule {
-}
+export class AppModule { }
