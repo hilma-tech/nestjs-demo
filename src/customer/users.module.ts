bced3a4cb30d528521541ba0440b8cb10aa7e49f
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-07-08 15:47:00 +0300

    f

--- src/customer/users.module.ts
+++ src/customer/users.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 
 import { UsersController } from './users.controller';
-import { User } from './user.entity';
+// import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { PassportModule } from '@nestjs/passport';
 import { jwtConstants } from '../common/constants';
